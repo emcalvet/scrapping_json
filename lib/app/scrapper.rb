f6824f54840e78e9b926af0735969b4c158822c7
Emma <emma.calvet.g@gmail.com>
2019-01-22 17:52:24 +0100

    scrapper v3

--- lib/app/scrapper.rb
+++ lib/app/scrapper.rb
@@ -1,6 +1,6 @@
 class Scrapper
 
-    # Récupère l'adresse email , prend en parametre le liens de la page de la mairie 
+    # Récupère l'adresse email, prend en paramètre les liens de la page de la mairie 
   def get_townhall_email(townhall_url)
     email = ""
     page = Nokogiri::HTML(open(townhall_url))
@@ -15,18 +15,18 @@ class Scrapper
     arr_names = []
     arr_email = []
 
-    # récupére la liste de tout les liens redirigeant vers les pages de chaque mairie
+    # récupère la liste de tous les liens redirigeant vers les pages de chaque mairie
     page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
     page.xpath('//a[contains(@href, "95")]/@href').each do |el|
       arr_url.push(el.value)
     end
 
-    # Boucle pour récupéré le nom de chaque ville
+    # Boucle pour récupérer le nom de chaque ville
     name = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
     name.xpath('//a[contains(@href, "95")]').each do |el|
       arr_names.push(el.text)
     end
-    # boucle pour formaté l'url @ récupéré chaque email @ cette url
+    # boucle pour formater l'url et récupérer chaque email de cette url
     arr_url.each do |el|
       el = el[1..-1]
       tmp_str = "https://www.annuaire-des-mairies.com" + el
@@ -35,7 +35,6 @@ class Scrapper
     # Création du hash final
     my_hash = arr_names.zip(arr_email).to_h
 
-    # Le print pour vérifié que c'est bon est la <3
     return my_hash
   end
 
@@ -43,15 +42,11 @@ class Scrapper
     
   hash = Scrapper.new.get_townhall_urls
   index = 1
-  # Creates a session. This will prompt the credential via command line for the
-  # first time and save it to config.json file for later usages.
-  # See this document to learn how to create config.json:
-  # https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
+
+  # Crée une session
   session = GoogleDrive::Session.from_config("config.json")
 
-  # First worksheet of
-  # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
-  # Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
+  # Copier le code de l'url d'un Google Sheet à toi à la place
   ws = session.spreadsheet_by_key("1vKFn5-PVW0hvlLQ5HH7FHTWmIxcK4_lkSPu1mvMQjwE").worksheets[0]
 
   hash.each_key do |key|
